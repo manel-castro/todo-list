69812a394bedd3d5c39bdf096ccc5006ac2cefaf
manel-castro <manel.castro39@gmail.com>
2026-03-28 16:39:58 +0100

    basic react frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: 24 }}>
+      <h1>Todo Frontend</h1>
+      <p>This is a minimal Vite + React app served with nginx.</p>
+    </div>
+  );
+}
